4d66ddcbdd417db41ab592c0c1eb439800be892a
Ken Shiraishi <kshiraishi22@gmail.com>
2020-04-01 17:14:04 -0400

    added two works to portfolio section

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,89 +1,92 @@
 let resumeData = {
-    "imagebaseurl":"https://ken-shiraishi.dev",
-    "name": "Ken Shiraishi",
-    "role": "Full-Stack web developer",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": " recently completing George Washington's Full-Stack boot camp program. I'm an experienced biotech professional turned web developer.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/ken-shiraishi/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/kshiraishi22",
-          "className":"fa fa-github"
-        },
-        // {
-        //   "name":"email",
-        //   "url":"http://twitter.com/rbhatia46",
-        //   "className":"fa fa-twitter"
-        // }
-      ],
-    // "aboutme":"I recently completed the George Washington University's Full-Stack boot camp program. ",
-    
-    // "work":[
-    //   {
-    //     "CompanyName":"Some Company",
-    //     "specialization":"Some specialization",
-    //     "MonthOfLeaving":"Jan",
-    //     "YearOfLeaving":"2018",
-    //     "Achievements":"Some Achievements"
-    //   },
-    //   {
-    //     "CompanyName":"Some Company",
-    //     "specialization":"Some specialization",
-    //     "MonthOfLeaving":"Jan",
-    //     "YearOfLeaving":"2018",
-    //     "Achievements":"Some Achievements"
-    //   }
-    // ],
-    // "skillsDescription":"Your skills here",
-    // "skills":[
-    //   {
-    //     "skillname":"HTML5"
-    //   },
-    //   {
-    //     "skillname":"CSS"
-    //   },
-    //   {
-    //     "skillname":"Reactjs"
-    //   }
-    // ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    // "testimonials":[
-    //   {
-    //     "description":"This is a sample testimonial",
-    //     "name":"Some technical guy"
-    //   },
-    //   {
-    //     "description":"This is a sample testimonial",
-    //     "name":"Some technical guy"
-    //   }
-    // ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://ken-shiraishi.dev",
+  name: "Ken Shiraishi",
+  role: "Full-Stack web developer",
+  // linkedinId: "Your LinkedIn Id",
+  // skypeid: "Your skypeid",
+  roleDescription:
+    " recently completing George Washington's Full-Stack boot camp program. I'm an experienced biotech professional turned web developer.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/ken-shiraishi/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "http://github.com/kshiraishi22",
+      className: "fa fa-github"
+    }
+    // {
+    //   "name":"email",
+    //   "url":"http://twitter.com/rbhatia46",
+    //   "className":"fa fa-twitter"
+    // }
+  ],
+  // "aboutme":"I recently completed the George Washington University's Full-Stack boot camp program. ",
+
+  // "work":[
+  //   {
+  //     "CompanyName":"Some Company",
+  //     "specialization":"Some specialization",
+  //     "MonthOfLeaving":"Jan",
+  //     "YearOfLeaving":"2018",
+  //     "Achievements":"Some Achievements"
+  //   },
+  //   {
+  //     "CompanyName":"Some Company",
+  //     "specialization":"Some specialization",
+  //     "MonthOfLeaving":"Jan",
+  //     "YearOfLeaving":"2018",
+  //     "Achievements":"Some Achievements"
+  //   }
+  // ],
+  // "skillsDescription":"Your skills here",
+  // "skills":[
+  //   {
+  //     "skillname":"HTML5"
+  //   },
+  //   {
+  //     "skillname":"CSS"
+  //   },
+  //   {
+  //     "skillname":"Reactjs"
+  //   }
+  // ],
+  portfolio: [
+    {
+      name: "Commute-Buddy",
+      description:
+        "A navigation app that allows users to have turn-by-turn directions as well as an interactive map. Features include the option to view multiple routes based on mode of travel or traffic, fuel usage and fuel cost for a given trip as well as health benefits if they decide to bike or walk.",
+      imgurl: "images/commute-buddy.png"
+    },
+    {
+      name: "Open Sesame",
+      description:
+        "A password generator that allows for users to attain a secure password simply by choosing the type of characters they desire and how long they want the password to be.",
+      imgurl: "images/password_generator.png"
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png"
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    }
+  ]
+  // "testimonials":[
+  //   {
+  //     "description":"This is a sample testimonial",
+  //     "name":"Some technical guy"
+  //   },
+  //   {
+  //     "description":"This is a sample testimonial",
+  //     "name":"Some technical guy"
+  //   }
+  // ]
+};
+
+export default resumeData;
