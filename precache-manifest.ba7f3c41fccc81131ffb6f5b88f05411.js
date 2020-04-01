6f8f078179a921dfd382ecbefd240d512ae17318
Ken Shiraishi <kshiraishi22@gmail.com>
2020-03-31 20:39:56 -0400

    Updates

---
+++ precache-manifest.ba7f3c41fccc81131ffb6f5b88f05411.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27970d1359fa75122512",
+    "url": "/static/css/main.9fdaaa95.chunk.css"
+  },
+  {
+    "revision": "27970d1359fa75122512",
+    "url": "/static/js/main.27970d13.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "061a4a7d6d8dc07873fac1336e33f9e8",
+    "url": "/index.html"
+  }
+];
