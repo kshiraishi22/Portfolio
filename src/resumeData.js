040c83f25075c0f386ebd90030f9c4fd7aac63a0
Ken Shiraishi <kshiraishi22@gmail.com>
2020-04-03 17:15:35 -0400

    fixed project image sizes and added links to project images

--- src/resumeData.js
+++ src/resumeData.js
@@ -56,31 +56,35 @@ let resumeData = {
     {
       name: "Commute-Buddy",
       description:
-        "A navigation app that allows users to have turn-by-turn directions as well as an interactive map. Features include the option to view multiple routes based on mode of travel or traffic, fuel usage and fuel cost for a given trip as well as health benefits if they decide to bike or walk.",
-      imgurl: "images/commute-buddy.png"
+        "A navigation app that allows users to have turn-by-turn directions as well as an interactive map. Features include the option to view multiple routes based on mode of travel or traffic, fuel usage and fuel cost for a given trip as well as health benefits for biking or walking.",
+      imgurl: "images/commute-buddy.png",
+      href: "https://magiama9.github.io/commute-buddy/"
 
     },
-    {
-      name: "Git Connected",
-      description:
-        "A job listing app that focuses primarily on employment seeking web developers to connect with employers looking to hire them. The app includes a secure login, ability to view/edit profiles, apply to jobs (developers) and post jobs (employers).",
-      imgurl: "images/git_connected.png"
-    },
+    // {
+    //   name: "Git Connected",
+    //   description:
+    //     "A job listing app that focuses primarily on employment seeking web developers to connect with employers looking to hire them. The app includes a secure login, ability to view/edit profiles, apply to jobs (developers) and post jobs (employers).",
+    //   imgurl: "images/git_connected.png"
+    // },
     {
       name: "Open Sesame",
       description:
         "A password generator that allows for users to attain a secure password simply by choosing the type of characters they desire and how long they want the password to be.",
-      imgurl: "images/password_generator.png"
+      imgurl: "images/password_generator.png",
+      href: "https://github.com/kshiraishi22/open_sesame"
     },
     {
       name: "Employee Directory",
       description: "A sample employee directory using React that allows users to search for employees based on name, phone #, email, or DOB.",
-      imgurl: "images/employee_directory.png"
+      imgurl: "images/employee_directory.png",
+      href: "https://arcane-tundra-09488.herokuapp.com/"
     },
     {
       name: "Burger Me Up",
       description: "A simple burger app that allows users to list out any type of burger they want and once they click on the corresponding button, the burger will move over to the devoured list. Data is saved through MySQL.",
-      imgurl: "images/burger_app.png"
+      imgurl: "images/burger_app.png",
+      href: "https://peaceful-crag-51873.herokuapp.com/home"
     }
   ]
   // "testimonials":[
